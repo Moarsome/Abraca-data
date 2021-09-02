71a7e1362fd66b369918af4c08e54f77d8a522f5
Kylie Crump <kyliecrump1@gmail.com>
2021-09-02 15:39:15 +1200

    eijfs

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ function parseHop(jsonHop, jsonConsumption, previousDate) {
             }
         }
     }
-
+    
     // Print highest off-peak power usage 
     alert("max interval: " + maxConsumption["consumption"] + " at " + maxConsumption["time"]);
 }
