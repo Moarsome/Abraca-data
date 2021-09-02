2831535d27f3109898c8bb04f14a7e88d7ed779a
Kylie Crump <kyliecrump1@gmail.com>
2021-09-03 10:56:43 +1200

    chart

--- graph.js
+++ graph.js
@@ -1,6 +1,49 @@
-function createChart(temperatureData, timeData, powerHour)
+function createWeatherChart(temperatureData, timeData, powerHour)
 {
-    var ctx = document.getElementById('myChart').getContext('2d');
+    var ctx = document.getElementById('weatherChart').getContext('2d');
+
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: timeData,
+            datasets: [
+                {
+                label: 'Temperatures (°C)',
+                data: temperatureData,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            },
+        ]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+
+}
+
+function createUsageChart(temperatureData, timeData, powerHour)
+{
+    var ctx = document.getElementById('weatherChart').getContext('2d');
 
     var myChart = new Chart(ctx, {
         type: 'line',
